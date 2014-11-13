e339bfa34416afdf7e53443a2104ec3a0ec8f22b
Denis Krienbühl <denis@href.ch>
2014-11-13 13:56:13 +0100

    Fix PEP8 warnings in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 from setuptools import setup, find_packages
 
 long_description = (
@@ -6,27 +5,30 @@ long_description = (
     + '\n' +
     open('CHANGES.txt').read())
 
-setup(name='more.static',
-      version='0.5.dev0',
-      description="BowerStatic integration for Morepath",
-      long_description=long_description,
-      author="Martijn Faassen",
-      author_email="faassen@startifact.com",
-      keywords='morepath bowerstatic bower',
-      license="BSD",
-      url="http://pypi.python.org/pypi/more.static",
-      namespace_packages=['more'],
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+setup(
+    name='more.static',
+    version='0.5.dev0',
+    description="BowerStatic integration for Morepath",
+    long_description=long_description,
+    author="Martijn Faassen",
+    author_email="faassen@startifact.com",
+    keywords='morepath bowerstatic bower',
+    license="BSD",
+    url="http://pypi.python.org/pypi/more.static",
+    namespace_packages=['more'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'setuptools',
         'morepath >= 0.7',
         'bowerstatic >= 0.4',
+    ],
+    extras_require=dict(
+        test=[
+            'pytest >= 2.0',
+            'pytest-cov',
+            'WebTest >= 2.0.14'
         ],
-      extras_require = dict(
-        test=['pytest >= 2.0',
-              'pytest-cov',
-              'WebTest >= 2.0.14'],
-        ),
-      )
+    ),
+)
