b890e4a1e8215e11b902b77ea4249d60fade44c4
Denis Krienbühl <denis@href.ch>
2014-11-13 14:42:45 +0100

    Change more.static to use a tween for injection

--- more/static/core.py
+++ more/static/core.py
@@ -1,3 +1,5 @@
+import bowerstatic
+
 from morepath.request import Request
 from morepath.reify import reify
 from morepath.app import App
@@ -11,17 +13,30 @@ def get_static_components():
 
 
 class IncludeRequest(Request):
-    @reify
-    def static_components(self):
-        return get_static_components(lookup=self.lookup)
 
     def include(self, path_or_resource):
-        include = self.static_components.includer(self.environ)
+        include = self.app.bower_components.includer(self.environ)
         include(path_or_resource)
 
 
 class StaticApp(App):
     request_class = IncludeRequest
+
+    @reify
+    def bower(self):
+        return bowerstatic.Bower()
+
+    @reify
+    def bower_components(self):
+        return get_static_components(lookup=self.lookup)
+
+    @reify
+    def bower_publisher(self):
+        return bowerstatic.publisher.Publisher(self.bower, None)
+
+    @reify
+    def bower_injector(self):
+        return bowerstatic.injector.Injector(self.bower, None)
 
 
 @StaticApp.directive('static_components')
@@ -32,3 +47,13 @@ class StaticComponentsDirective(Directive):
 
     def perform(self, registry, obj):
         registry.register_function(get_static_components, obj)
+
+
+@StaticApp.tween_factory()
+def get_bower_injector_tween(app, handler):
+    def bower_injector_tween(request):
+        response = handler(request)
+        response = request.app.bower_injector.inject(request, response)
+        response = request.app.bower_publisher.publish(request, response)
+        return response
+    return bower_injector_tween
