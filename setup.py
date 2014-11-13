20edf303c6c03e4b411bc6c69b008d72059f46a1
faassen <faassen@startifact.com>
2014-11-13 16:23:44 +0100

    Preparing release 0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
 
 setup(
     name='more.static',
-    version='0.5.dev0',
+    version='0.5',
     description="BowerStatic integration for Morepath",
     long_description=long_description,
     author="Martijn Faassen",
